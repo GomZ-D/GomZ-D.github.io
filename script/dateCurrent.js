0f58ad77befa7ba370dc3eec78ce29edcb8e03f3
GomZ_D <gomezdiego09@gmail.com>
2022-07-16 18:02:09 -0400

    errors Check Weather Page

--- script/dateCurrent.js
+++ script/dateCurrent.js
@@ -1,8 +1,11 @@
 let olastModif = document.lastModified;
 let nLastModif = Date.parse(document.lastModified);
-
-let dayN
-
-console.log(olastModif)
+let year = new Date(nLastModif)
+// let dayN = nLastModif.getYear()
+let dayN = year.getFullYear()
+console.log(dayN)
+// console.log('hola')
+// 
 
 document.querySelector("#currentDate").innerHTML = ' ' + olastModif;
+document.querySelector("#currentYear").innerHTML = dayN;
--- weather/JS/dateCurrent.js
+++ weather/JS/dateCurrent.js
@@ -2,6 +2,7 @@ const month = ["January","February","March","April","May","June","July","August"
 const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
  
 let myDate = new Date()
+// let year = myDate.getFullYear()
  
 let diaName = weekday[myDate.getDay()];
 let dia2 = myDate.getDate();
@@ -10,4 +11,5 @@ let year = myDate.getFullYear();
 let fullDate = ' '+diaName +', '+dia2+' '+monthName+ ' '+year;
 console.log(fullDate)
 document.querySelector("#currentDate").textContent = fullDate; 
+document.querySelector("#currentYear").innerHTML = year;
  
